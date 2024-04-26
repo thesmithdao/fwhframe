2996414cc7b12408ecac9114308316f8e0ac7dad
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-04-26 18:58:52 -0300

    fix: try to update website url env

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { fetchMetadata } from "frames.js/next"
 import { Metadata } from "next"
 
 export const WEBSITE_URL =
-  process.env.FOX_WEBSITE_URL || "http://localhost:3000"
+  process.env.WEBSITE_URL || "http://localhost:3000"
 
 console.log({ WEBSITE_URL })
 
