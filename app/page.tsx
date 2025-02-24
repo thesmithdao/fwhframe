1c0886f3ac80109c1c1f79c18baaa3d985d90ad6
TheSmith <43218493+thesmithdao@users.noreply.github.com>
2025-02-23 21:28:29 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { fetchMetadata } from "frames.js/next";
 import { Metadata } from "next";
 
-const WEBSITE_URL = process.env.FWH_WEBSITE_URL || "https://fwhframe.vercel.app/frames";
+const WEBSITE_URL = process.env.FWH_WEBSITE_URL || "https://fwhframe.vercel.app/";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
