bcc863227c1ac48a6293bb249cd6d0fc8d2987fb
TheSmith <43218493+thesmithdao@users.noreply.github.com>
2025-02-23 21:16:52 -0300

    Update route.tsx

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -1,15 +1,26 @@
-import { ABI } from "@/lib/abi";
-import { FWH_CONTRACT } from "@/lib/constants";
-import { supabase } from "@/lib/supabase";
-import { checkInteractionTime } from "@/lib/utils";
-import { account, publicClient, walletClient } from "@/lib/web3-client";
-import { Button, createFrames } from "frames.js/next";
-import { CSSProperties } from "react";
-import { parseUnits } from "viem";
+import { ABI } from "@/lib/abi"
+import { FWH_CONTRACT } from "@/lib/constants"
+import { supabase } from "@/lib/supabase"
+import { checkInteractionTime } from "@/lib/utils"
+import { account, publicClient, walletClient } from "@/lib/web3-client"
+import { farcasterHubContext } from "frames.js/middleware"
+import { Button, createFrames } from "frames.js/next"
+import { CSSProperties } from "react"
+import { parseUnits } from "viem"
 
 const frames = createFrames({
   basePath: "/frames",
-});
+  middleware: [
+    farcasterHubContext({
+      // remove if you aren't using @frames.js/debugger or you just don't want to use the debugger hub
+      ...(process.env.NODE_ENV === "production"
+        ? {}
+        : {
+            hubHttpUrl: "http://localhost:3010/hub",
+          }),
+    }),
+  ],
+})
 
 const div_style: CSSProperties = {
   display: "flex",
@@ -22,11 +33,12 @@ const div_style: CSSProperties = {
   color: "#00FF41",
   textShadow: "0 0 4px #00FF41,0 0 5px #00FF41,0 0 5px #00FF41",
   filter: "blur(0.02rem)",
-};
+}
 
 const handleRequest = frames(async (ctx) => {
-  const message = ctx.message as any;
+  const message = ctx.message
 
+  // If no message, show home page
   if (!message)
     return {
       image:
@@ -36,9 +48,31 @@ const handleRequest = frames(async (ctx) => {
           🦊 Claim FWH
         </Button>,
       ],
-    };
+    }
 
-  if (!message.requesterVerifiedAddresses || !message.requesterVerifiedAddresses.length) {
+  let image = ""
+
+  if (!message.likedCast && !message.requesterFollowsCaster) {
+    image = "https://github.com/r4topunk/shapeshift-faucet-frame/blob/main/public/fox_follow_like.png?raw=true"
+  } else if (!message.likedCast) {
+    image = "https://github.com/r4topunk/shapeshift-faucet-frame/blob/main/public/fox_follow.png?raw=true"
+  } else {
+    image = "https://github.com/r4topunk/shapeshift-faucet-frame/blob/main/public/fox_like.png?raw=true"
+  }
+
+  // If user didn't complete the requirements, show to do list
+  if (!message.likedCast || !message.requesterFollowsCaster) {
+    return {
+      image: image,
+      buttons: [
+        <Button action="post" target={{ query: { state: true } }}>
+          Try again
+        </Button>,
+      ],
+    }
+  }
+
+  if (!message.requesterVerifiedAddresses.length) {
     return {
       image: (
         <div style={div_style}>
@@ -50,50 +84,35 @@ const handleRequest = frames(async (ctx) => {
           Try again
         </Button>,
       ],
-    };
-  }
-
-  const userAddress: `0x${string}` = message.requesterVerifiedAddresses[0] as `0x${string}`;
-
-  if (!userAddress) {
-    return {
-      image: (
-        <div style={div_style}>
-          You don't have a Verified Address added to Farcaster.
-        </div>
-      ),
-      buttons: [
-        <Button action="post" target={{ query: { state: true } }}>
-          Try again
-        </Button>,
-      ],
-    };
+    }
   }
 
   // Find user last claim
-  const { data } = await supabase
+  const { data, error } = await supabase
     .from("fwh_claims")
-    .select("claimed_at")
-    .eq("eth_address", userAddress)
+    .select("claimed_at", { count: "exact" })
+    .eq("fid", message?.requesterFid)
     .order("claimed_at", { ascending: false })
-    .limit(1);
+    .limit(1)
+  const lastInteractionTime = checkInteractionTime(data)
 
-  const lastInteractionTime = checkInteractionTime(data);
-
+  // If find claims or has not passed 24 hours since last claim
   if (lastInteractionTime && !lastInteractionTime.has24HoursPassed) {
+    const buttonText = `Try again in ${lastInteractionTime.formattedTime}`
     return {
-      image:
-        "https://github.com/thesmithdao/fwhframe/blob/main/public/wait.png?raw=true",
+      image: "https://github.com/thesmithdao/fwhframe/blob/main/public/wait.png?raw=true",
       buttons: [
         <Button action="post" target={{ query: { state: true } }}>
-          {`Try again in ${lastInteractionTime.formattedTime}`}
+          {buttonText}
         </Button>,
       ],
-    };
+    }
   }
 
+  const userAddress = message.requesterVerifiedAddresses[0] as `0x${string}`
+
   // send transaction
-  let receipt = "";
+  let receipt = ""
   try {
     const { request } = await publicClient.simulateContract({
       account,
@@ -101,9 +120,10 @@ const handleRequest = frames(async (ctx) => {
       abi: ABI,
       functionName: "transfer",
       args: [userAddress, parseUnits("0.000333", 18)],
-    });
-    receipt = await walletClient.writeContract(request);
+    })
+    receipt = await walletClient.writeContract(request)
   } catch (e: any) {
+    console.error(e)
     return {
       image: (
         <div
@@ -113,27 +133,29 @@ const handleRequest = frames(async (ctx) => {
           <span>{e.message}</span>
         </div>
       ),
-    };
+    }
   }
 
   // Save claim history
-  await supabase.from("fwh_claims").insert({
-    fid: message?.requesterFid || 0,
-    f_address: message?.requesterVerifiedAddresses?.[0] || "N/A",
+  const save = await supabase.from("fwh_claims").insert({
+    fid: message?.requesterFid,
+    f_address: message?.requesterCustodyAddress,
     eth_address: userAddress,
-    claimed_at: new Date().toISOString(),
-  });
+  })
 
   return {
     image:
       "https://github.com/thesmithdao/fwhframe/blob/main/public/claimed.png?raw=true",
     buttons: [
-      <Button action="link" target={`https://basescan.org/tx/${receipt}`}>
+      <Button
+        action="link"
+        target={`https://basescan.org/tx/${receipt}`}
+      >
         See on Base Scan
       </Button>,
     ],
-  };
-});
+  }
+})
 
-export const GET = handleRequest;
-export const POST = handleRequest;
+export const GET = handleRequest
+export const POST = handleRequest
